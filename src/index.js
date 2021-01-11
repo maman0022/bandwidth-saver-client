72ea20507add1a1fbb3b07d21f43af6750e9ad4a
Muhammad Aman <maman0022@outlook.com>
2021-01-11 09:09:57 -0600

    removed semicolons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css';
+import './index.css'
 import 'normalize.css'
-import App from './components/App/App';
+import App from './components/App/App'
 
 ReactDOM.render(
   <BrowserRouter>
@@ -12,4 +12,4 @@ ReactDOM.render(
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById('root')
-);
+)
